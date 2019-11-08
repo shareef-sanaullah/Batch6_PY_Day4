85456548e3e4ca9268d23ed4ee66dc6eb2196df2
shareef-sanaullah <57405273+shareef-sanaullah@users.noreply.github.com>
2019-11-08 21:28:05 +0300

    Add files via upload

---
+++ Batch6_PY_Day4.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+my_students = ['junaid','adil','naveen','farhan']
+
+print(my_students)
+
+
+# In[2]:
+
+
+my_students[0] = 'tariq'
+
+print(my_students)
+
+
+# In[3]:
+
+
+my_students.append('satish')
+
+print(my_students)
+
+
+# In[4]:
+
+
+my_students.insert(2,'asif')
+
+print(my_students)
+
+
+# In[5]:
+
+
+del my_students[4]
+
+print(my_students)
+
+
+# In[6]:
+
+
+my_students.pop()
+
+
+# In[7]:
+
+
+print(my_students)
+
+
+# In[8]:
+
+
+team = ['john','asim','karthik','haseeb']
+
+print(team)
+
+
+# In[9]:
+
+
+print(team[0:2])
+
+
+# In[10]:
+
+
+print(team[2:4])
+
+
+# In[11]:
+
+
+print(team[0:2:4])
+
+
+# In[ ]:
+
+
+
+
